77c83336f3000a32b2d2f3e06910ba8e9a980a01
李会锋 <1094129209@qq.com>
2016-07-09 09:23:17 +0800

    测试

--- project/controllers/LoginController.php
+++ project/controllers/LoginController.php
@@ -3,15 +3,19 @@
 namespace app\controllers;
 
 use yii\web\Controller;
+use app\models\User;
+use app\models\Limit_ip;
 
 class LoginController extends Controller{
 
     public $layout='common';
+    public $enableCsrfValidation = false;
 
     /*
      * 登录页面
      */
     public function actionIndex(){
+    	echo 3;
         return $this->render('login.html');
     }
 
@@ -19,6 +23,20 @@ class LoginController extends Controller{
      * 检测登录
      */
     function actionCheck(){
-        return $this->redirect(array('/backstage/index'));
+        //获取登录数据
+        $request = \Yii::$app->request;
+        $login_info = $request->post();
+        $ip = \Yii::$app->request->userIP;
+        //判断ip是否在限制范围内
+        if(Limit_ip::find()->where(['ip'=>$ip])->one()){
+            //验证账号密码
+            if(User::find()->where(['email'=>$login_info['email'],'pwd'=>$login_info['password']])->one()){
+                echo 1;
+            }else{
+                echo 0;
+            }
+        }else{
+            echo 2;
+        }
     }
 }
