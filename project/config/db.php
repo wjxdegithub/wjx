cd212d6347a9505e553a885f6829fbe1dd6138e6
李会锋 <1094129209@qq.com>
2016-07-10 19:25:55 +0800

    更新版本1

--- project/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=weiqin',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
